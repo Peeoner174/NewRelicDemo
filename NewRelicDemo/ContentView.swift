8ad549731c7cb327fbb329c5bce92be8c9915223
Pavel Kochenda <p.kochenda@prequelapp.com>
2023-10-24 16:00:15 +0500

    Initial Commit

---
+++ NewRelicDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewRelicDemo
+//
+//  Created by Pavel Kochenda on 24/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewRelicDemo/NewRelicDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewRelicDemoApp.swift
+//  NewRelicDemo
+//
+//  Created by Pavel Kochenda on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewRelicDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
