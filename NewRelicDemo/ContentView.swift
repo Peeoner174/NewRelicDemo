37040981374c04bafda46fefa597e9c5cd679b79
Pavel Kochenda <p.kochenda@prequelapp.com>
2023-10-26 01:20:00 +0500

    [fix]wip project compability with xcode 13

--- NewRelicDemo/ContentView.swift
+++ NewRelicDemo/ContentView.swift
@@ -9,16 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 15) {
+            Button(action: {
+                executeNetworkRequest()
+            }) {
+                Text("EXECUTE REQUEST")
+                    .font(.title)
+                    .padding()
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+            Button(action: {
+                throwException()
+            }) {
+                Text("THROW EXCEPTION")
+                    .font(.title)
+                    .padding()
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
         }
-        .padding()
+        .padding([.leading, .trailing], 15)
+    }
+    
+    private func executeNetworkRequest() {
+        let url = URL(string: "https://google.com")!
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { _, _, error in
+            switch error {
+            case .none:
+                print("Request Success")
+            case .some:
+                print("Request failed")
+            }
+        }
+        .resume()
+    }
+    
+    private func throwException() {
+        fatalError("Test Exception")
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- NewRelicDemo/NewRelicDemoApp.swift
+++ NewRelicDemo/NewRelicDemoApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import NewRelic
 
 @main
 struct NewRelicDemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        setupNewRelic()
+        
+        return true
+    }
+
+    private func setupNewRelic() {
+        NRLogger.setLogLevels(NRLogLevelAudit.rawValue)
+
+        NewRelic
+            .start(
+                withApplicationToken: "eu01xxcd1e296feb5e74806ca988e6534522cbed40-NRMA" // ,
+//              andCollectorAddress: "collector-dev.tseries.ru",
+//              andCrashCollectorAddress: "collector-dev.tseries.ru"
+            )
     }
 }
